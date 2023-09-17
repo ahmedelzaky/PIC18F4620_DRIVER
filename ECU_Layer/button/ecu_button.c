c4c26f2b78c440abf10409264a83e519fcfa1244
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-09-18 01:25:38 +0300

    button interface

---
+++ ECU_Layer/button/ecu_button.c
@@ -0,0 +1,83 @@
+/* 
+ * File:   ecu_button.c
+ * Author: zekwa
+ *
+ * Created on September 17, 2023, 10:26 PM
+ */
+#include "ecu_button.h"
+
+/**
+ * 
+ * @param btn
+ * @return button pin configuration 
+ */
+static pin_config_t btton_create_pin(button_t *btn)
+{
+    pin_config_t btn_pin = {
+        .port = btn->port,
+        .pin = btn->pin,
+        .direction = INPUT,
+        .logic = btn->btn_connection ? LOW : HIGH,
+    };
+
+    return btn_pin;
+}
+
+/**
+ * 
+ * @param btn
+ * @return  Status of the function
+ *         (E_OK) : The function done successfully
+ *         (E_NOT_OK) : The function has issue to perform this actionn
+ */
+Std_ReturnType button_initialize(button_t *btn)
+{
+    Std_ReturnType ret = E_OK;
+    if (btn == NULL)
+    {
+        ret = E_NOT_OK;
+    }
+    else
+    {
+        pin_config_t btn_pin = btton_create_pin(btn);
+        ret = gpio_pin_direction_intialize(&btn_pin);
+    }
+    return ret;
+}
+
+/**
+ * 
+ * @param btn
+ * @param state
+ * @return  Status of the function
+ *         (E_OK) : The function done successfully
+ *         (E_NOT_OK) : The function has issue to perform this actionn
+ */
+Std_ReturnType button_read_state(button_t *btn, button_state_t *state)
+{
+    Std_ReturnType ret = E_OK;
+    logic_t pin_logic;
+    if (btn == NULL || state == NULL)
+    {
+        ret = E_NOT_OK;
+    }
+    else
+    {
+        pin_config_t btn_pin = btton_create_pin(btn);
+        ret = gpio_pin_read_logic(&btn_pin, &pin_logic);
+        if (btn->btn_connection == BUTTON_PULLDOWN)
+        {
+            pin_logic == HIGH ? (*state = BUTTON_PRESSED) : (*state = BUTTON_RELEASED);
+        }
+        else if (btn->btn_connection == BUTTON_PULLUP)
+        {
+            pin_logic == LOW ? (*state = BUTTON_PRESSED) : (*state = BUTTON_RELEASED);
+        }
+        else
+        {
+            //NOTHING
+        }
+    }
+    return ret;
+
+}
---
+++ ECU_Layer/button/ecu_button.h
@@ -0,0 +1,47 @@
+/* 
+ * File:   ecu_button.h
+ * Author: zekwa
+ *
+ * Created on September 17, 2023, 10:26 PM
+ */
+
+#ifndef ECU_BUTTON_H
+#define	ECU_BUTTON_H
+
+/* Section: Includes */
+
+#include "../../MCAL_Layer/GPIO/hal_gpio.h"
+
+
+/* Section: Macros */
+
+/* Section: Macro Functions */
+
+/* Section: Data Type Declaration */
+
+typedef enum {
+    BUTTON_RELEASED = 0,
+    BUTTON_PRESSED,
+} button_state_t;
+
+typedef enum {
+    BUTTON_PULLUP = 0,
+    BUTTON_PULLDOWN,
+} button_connection_t;
+
+typedef struct {
+    uint8_t port : 4; /* @ref  port_index_t */
+    uint8_t pin : 3; /* @ref  pin_index_t */
+    uint8_t btn_connection : 1; /* @ref button_connection_t */
+} button_t;
+
+
+/* Section: Function Declaration */
+static pin_config_t btton_create_pin(button_t *btn);
+Std_ReturnType button_initialize(button_t *btn);
+Std_ReturnType button_read_state(button_t *btn, button_state_t *state);
+
+
+
+#endif	/* ECU_BUTTON_H */
+
