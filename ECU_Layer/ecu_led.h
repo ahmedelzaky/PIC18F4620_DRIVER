72b1d7d0022a8c249ba704917a104e2c972255ee
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-09-17 22:10:05 +0300

    led interface

--- ECU_Layer/ecu_led.h
+++ ECU_Layer/ecu_led.h
@@ -11,15 +11,33 @@
 /* Section: Includes */
 
 #include "../MCAL_Layer/GPIO/hal_gpio.h"
+#include "ecu_led_config.h"
 
 
 /* Section: Macros */
 
 /* Section: Macro Functions */
 
-/* Section: Variables */
+/* Section: Data Type Declaration */
+
+typedef enum {
+    LED_OFF = 0,
+    LED_ON,
+} led_status_t;
+
+typedef struct {
+    uint8_t port : 4; /* @ref  port_index_t */
+    uint8_t pin : 3; /* @ref  pin_index_t */
+    uint8_t led_status : 1; /* @ref led_status_t */
+} led_t;
+
 
 /* Section: Function Declaration */
+static pin_config_t led_pin(led_t *led);
+Std_ReturnType led_initialize(led_t *led);
+Std_ReturnType led_turn_on(led_t *led);
+Std_ReturnType led_turn_off(led_t *led);
+Std_ReturnType led_turn_toggle(led_t *led);
 
 #endif	/* ECU_LED_H */
 
