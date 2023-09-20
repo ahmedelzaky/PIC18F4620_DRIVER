561227518882c57c4ec06167630cc65db1b558dc
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-09-21 00:56:16 +0300

    dc motor interface

---
+++ ECU_Layer/dc_motor/dc_motor.c
@@ -0,0 +1,88 @@
+/* 
+ * File:   dc_motor.h
+ * Author: zekwa
+ *
+ * Created on September 20, 2023, 10:41 PM
+ */
+
+#include "dc_motor.h"
+
+static pin_config_t motor_create_pin(dc_motor_pin_t *motor_pin)
+{
+    pin_config_t motor_pin_config = {
+        .port = motor_pin->port,
+        .pin = motor_pin->pin,
+        .direction = OUTPUT,
+        .logic = motor_pin->state,
+    };
+    return motor_pin_config;
+}
+
+Std_ReturnType motor_initialize(dc_motor_t *motor)
+{
+    Std_ReturnType ret = E_OK;
+    if (motor == NULL)
+    {
+        ret = E_NOT_OK;
+    }
+    else
+    {
+        pin_config_t motor_pin_1 = motor_create_pin(&(motor->pin_1));
+        ret = gpio_pin_intialize(&motor_pin_1);
+        pin_config_t motor_pin_2 = motor_create_pin(&(motor->pin_2));
+        ret = gpio_pin_intialize(&motor_pin_2);
+    }
+    return ret;
+}
+
+Std_ReturnType motor_move_left(dc_motor_t *motor)
+{
+    Std_ReturnType ret = E_OK;
+    if (motor == NULL)
+    {
+        ret = E_NOT_OK;
+    }
+    else
+    {
+        pin_config_t motor_pin_1 = motor_create_pin(&(motor->pin_1));
+        ret = gpio_pin_write_logic(&motor_pin_1, HIGH);
+        pin_config_t motor_pin_2 = motor_create_pin(&(motor->pin_2));
+        ret = gpio_pin_write_logic(&motor_pin_2, LOW);
+    }
+    return ret;
+}
+
+Std_ReturnType motor_move_right(dc_motor_t *motor)
+{
+    Std_ReturnType ret = E_OK;
+    if (motor == NULL)
+    {
+        ret = E_NOT_OK;
+    }
+    else
+    {
+        pin_config_t motor_pin_1 = motor_create_pin(&(motor->pin_1));
+        ret = gpio_pin_write_logic(&motor_pin_1, LOW);
+        pin_config_t motor_pin_2 = motor_create_pin(&(motor->pin_2));
+        ret = gpio_pin_write_logic(&motor_pin_2, HIGH);
+    }
+    return ret;
+}
+
+Std_ReturnType motor_stop(dc_motor_t *motor)
+{
+    Std_ReturnType ret = E_OK;
+    if (motor == NULL)
+    {
+        ret = E_NOT_OK;
+    }
+    else
+    {
+        pin_config_t motor_pin_1 = motor_create_pin(&(motor->pin_1));
+        ret = gpio_pin_write_logic(&motor_pin_1, LOW);
+        pin_config_t motor_pin_2 = motor_create_pin(&(motor->pin_2));
+        ret = gpio_pin_write_logic(&motor_pin_2, LOW);
+    }
+    return ret;
+}
+
---
+++ ECU_Layer/dc_motor/dc_motor.h
@@ -0,0 +1,46 @@
+/* 
+ * File:   dc_motor.h
+ * Author: zekwa
+ *
+ * Created on September 20, 2023, 10:41 PM
+ */
+
+#ifndef DC_MOTOR_H
+#define	DC_MOTOR_H
+
+/* Section: Includes */
+
+#include "../../MCAL_Layer/GPIO/hal_gpio.h"
+
+
+/* Section: Macros */
+
+/* Section: Data Type Declaration */
+
+typedef enum {
+    MOTOR_OFF = 0,
+    MOTOR_ON
+} dc_motor_state_t;
+
+typedef struct {
+    uint8_t port : 4; /* @ref  port_index_t */
+    uint8_t pin : 3; /* @ref  pin_index_t */
+    uint8_t state : 1; /* @ref motor_state_t */
+} dc_motor_pin_t;
+
+typedef struct {
+    dc_motor_pin_t pin_1; /* @ref motor_pin_t */
+    dc_motor_pin_t pin_2; /* @ref motor_pin_t */
+} dc_motor_t;
+
+
+/* Section: Function Declaration */
+static pin_config_t motor_create_pin(dc_motor_pin_t *motor);
+Std_ReturnType motor_initialize(dc_motor_t *motor);
+Std_ReturnType motor_move_left(dc_motor_t *motor);
+Std_ReturnType motor_move_right(dc_motor_t *motor);
+Std_ReturnType motor_stop(dc_motor_t *motor);
+
+
+#endif	/* DC_MOTOR_H */
+
