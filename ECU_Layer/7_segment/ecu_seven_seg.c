f34e2a2f8011c828935991bf529c66428085a05f
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-09-24 15:55:52 +0300

    hot fix

--- ECU_Layer/7_segment/ecu_seven_seg.c
+++ ECU_Layer/7_segment/ecu_seven_seg.c
@@ -25,8 +25,8 @@ static pin_config_t *segment_create_pins(segment_t *segment)
     segment_pins[2].port = segment->pin_C.port;
     segment_pins[2].pin = segment->pin_C.pin;
     segment_pins[2].logic = segment->pin_C.state;
-    segment_pins[2].direction = OUTPUT;
-
+    
+    segment_pins[3].direction = OUTPUT;
     segment_pins[3].port = segment->pin_D.port;
     segment_pins[3].pin = segment->pin_D.pin;
     segment_pins[3].logic = segment->pin_D.state;
